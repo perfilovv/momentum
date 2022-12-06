ae8f02ec60aef1aaf70308dcb076c6bad5ba9a5b
Vadim <russianspiritoneshotlol@gmail.com>
2022-12-06 14:35:33 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -3,12 +3,12 @@ function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
   time.textContent = currentTime;
-  setTimeout(showTime, 1000);
+
   showDate();
+  showGreeting();
+  setTimeout(showTime, 1000);
 }
 showTime();
-
-
 
 function showDate() {
   const data = document.querySelector('.date');
@@ -18,3 +18,40 @@ function showDate() {
   data.textContent = currentDate;
 }
 showDate();
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  if (hours < 6) {
+    return 'night';
+  } else if (hours < 12) {
+    return 'morning';
+  } else if (hours < 18) {
+    return 'afternoon';
+  } else {
+    return 'evening';
+  }
+}
+getTimeOfDay();
+
+function showGreeting() {
+  const greeting = document.querySelector('.greeting');
+  const timeOfDay = getTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  greeting.textContent = greetingText;
+}
+showGreeting();
+
+function setLocalStorage() {
+  const name = document.querySelector('.name');
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  const name = document.querySelector('.name');
+  if (localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
